44f65c345f80ecf03330fe07ccd86650349daaeb
Martin Pham <tuyentq2009@gmail.com>
2020-04-15 00:51:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <h1>
-    Ohh!!! It's working!
+    Ohh!!! It's working!!!
   </h1>,
   rootElement
 )
