029c0224fc4eb8f902a3fd60e87bb4a975ebab08
Martin Pham <tuyentq2009@gmail.com>
2020-04-14 23:48:56 +0200

    no message

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <h1>
+    Ops!!!
+  </h1>,
+  rootElement
+)
