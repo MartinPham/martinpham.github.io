c771313b77abd3cf8ffbf388aeaf2cc8f1b40de7
Martin Pham <tuyentq2009@gmail.com>
2020-04-15 11:49:33 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,11 @@
+import React, {useState} from 'react';
 
+export default () => {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <button
+      onClick={() => setCount(count + 1)}
+    >{count === 0 ? 'Click me' : `Clicked ${count} time${count > 1 ? 's' : ''}`}</button>
+  )
+}
